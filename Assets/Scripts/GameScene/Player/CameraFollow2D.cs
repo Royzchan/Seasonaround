49d1a3009603a8396450521497132a9ac1a1f614
林　利樹 <117792737+Royzchan@users.noreply.github.com>
2024-10-23 12:30:31 +0900

    ChangeCamera

--- Assets/Scripts/GameScene/Player/CameraFollow2D.cs
+++ Assets/Scripts/GameScene/Player/CameraFollow2D.cs
@@ -10,8 +10,10 @@ public class CameraFollow2D : MonoBehaviour
     private float _followSpeed;
 
     GameObject _player;//プレイヤー
+    PlayerController_2D _playerController2D;
 
     Vector3 _offset;//初期のプレイヤーとカメラの差
+    Vector3 _zoomOffset;
     Vector3 _pos;
 
     float _firstDis;//最初の距離
@@ -22,10 +24,18 @@ public class CameraFollow2D : MonoBehaviour
 
     bool _isCameraShake = false;
 
+    enum State
+    {
+        Follow,
+        Zoom
+    }
+    State _state = State.Follow;
+
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.FindWithTag("Player");
+        _playerController2D = _player.GetComponent<PlayerController_2D>();
         _offset = _player.transform.position - this.transform.position;
         _pos = transform.position;
         _firstDis = Vector3.Distance(_player.transform.position, transform.position);
@@ -33,7 +43,20 @@ public class CameraFollow2D : MonoBehaviour
 
     private void Update()
     {
+        //デバッグ
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Zoom(2);
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            ZoomOut();
+        }
 
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Shake(5, 0.1f);
+        }
     }
 
     // Update is called once per frame
@@ -41,49 +64,78 @@ public class CameraFollow2D : MonoBehaviour
     {
         if (_player != null)
         {
-            // カメラの追従
-            _pos.x = _player.transform.position.x - _offset.x;
-
-            //_posの位置に移動
-            transform.position = Vector3.Lerp(transform.position, _pos, _followSpeed);
+            if (_state == State.Follow)
+            {
+                FollowUpdate();
+            }
+            else if (_state == State.Zoom)
+            {
+                ZoomUpdate();
+            }
         }
     }
 
-    public IEnumerator Zoom(float value)
+    void FollowUpdate()
     {
-        if (_isZoom) yield break;
-
-        //現在の距離を計算
-        _dis = Vector3.Distance(_player.transform.position, transform.position);
-
-        while (_firstDis - value < _dis)
+        if (_isZoom)
         {
-            //現在の距離を計算
-            _dis = Vector3.Distance(_player.transform.position, transform.position);
-            //ズーム
-            _pos.z += _zoomSpeed * Time.deltaTime;
-            yield return null;
+            // カメラの追従
+            if (_playerController2D.ISFly)
+            {
+                _pos.y = _player.transform.position.y - _offset.y;
+            }
+            _pos.x = _player.transform.position.x - _zoomOffset.x;
+        }
+        else
+        {
+            // カメラの追従
+            if (_playerController2D.ISFly)
+            {
+                _pos.y = _player.transform.position.y - _offset.y;
+            }
+            _pos.x = _player.transform.position.x - _offset.x;
+        }
+        //_posの位置に移動
+        transform.position = Vector3.Lerp(transform.position, _pos, _followSpeed);
+    }
+
+    void ZoomUpdate()
+    {
+        if (_isZoom)
+        {
+            _pos.x = _player.transform.position.x;
+            if (_playerController2D.ISFly) _pos.y = _player.transform.position.y - _offset.y;
+        }
+        else
+        {
+            _pos.x = _player.transform.position.x - _offset.x;
+            if (_playerController2D.ISFly) _pos.y = _player.transform.position.y - _offset.y;
         }
 
+        //_posの位置に移動
+        transform.position = Vector3.Lerp(transform.position, _pos, _zoomSpeed);
+        if (transform.position == _pos)
+        {
+            _state = State.Follow;
+            _zoomOffset = transform.position - transform.position;
+        }
+    }
+
+    public void Zoom(float value)
+    {
+        if (_isZoom) return;
+
+        _state = State.Zoom;
+        _pos.z = _player.transform.position.z - _offset.z + value;
         //ズーム中に変更
         _isZoom = true;
     }
 
-    public IEnumerator ZoomOut()
+    public void ZoomOut()
     {
-        if (!_isZoom) yield break;
+        if (!_isZoom) return;
 
-        //現在の距離を計算
-        _dis = Vector3.Distance(_player.transform.position, transform.position);
-
-        while (_firstDis > _dis)
-        {
-            //現在の距離を計算
-            _dis = Vector3.Distance(_player.transform.position, transform.position);
-            //ズームアウト
-            _pos.z -= _zoomSpeed * Time.deltaTime;
-            yield return null;
-        }
+        _pos.z = _player.transform.position.z - _offset.z;
 
         //ズーム中ではない状態に変更
         _isZoom = false;
