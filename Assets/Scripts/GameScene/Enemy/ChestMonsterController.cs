8772bb0153308a8598c81e2cc04d7277d1f282fd
林　利樹 <117792737+Royzchan@users.noreply.github.com>
2025-01-10 13:35:18 +0900

    ミミックの敵のアニメーション作成

--- Assets/Scripts/GameScene/Enemy/ChestMonsterController.cs
+++ Assets/Scripts/GameScene/Enemy/ChestMonsterController.cs
@@ -7,8 +7,9 @@ public class ChestMonsterController : MonoBehaviour
     public enum State
     {
         Idle,   //‘Ò‹@
-        Move,   //ˆÚ“®
-        Attack  //UŒ‚
+        Run,    //ˆÚ“®
+        Attack, //UŒ‚
+        Search  //’T‚·
     }
 
     private Rigidbody _rb;
@@ -19,16 +20,23 @@ public class ChestMonsterController : MonoBehaviour
 
     [SerializeField, Header("ˆÚ“®‘¬“x")]
     private float _speed = 5.0f;
-    [SerializeField, Header("ˆÚ“®‚ðŽn‚ß‚é‹——£")]
+    [SerializeField, Header("UŒ‚‚ðŽn‚ß‚é‹——£")]
     private float _attackDistance = 2.0f;
     [SerializeField, Header("‰½•b“®‚¢‚½‚çUŒ‚‚·‚é‚©")]
-    private float _attackTime = 2.0f;
+    private float _attackTime = 1.5f;
+    [SerializeField, Header("UŒ‚Œã“G‚ð’T‚·ŽžŠÔ")]
+    private float _serachPlayerTime = 2.0f;
+
+    private bool _foundPlayer = false;
+    private float _runTimeCount = 0.0f;
+    private float _searchTimeCount = 0.0f;
 
     //ƒAƒjƒ[ƒVƒ‡ƒ“‚Ìstring
-    private string _idleChestStr;
-    private string _idleNormalStr;
-    private string _walkStr;
-    private string _runStr;
+    private string _isIdle = "isIdleChest";
+    private string _isDiscovery = "isDiscovery";
+    private string _isRun = "isRun";
+    private string _isAttack = "isAttack";
+    private string _isSearch = "isSearch";
 
     void Start()
     {
@@ -45,25 +53,93 @@ public class ChestMonsterController : MonoBehaviour
                 CheckPlayerDistance();
                 break;
 
-            case State.Move:
+            case State.Run:
+                _runTimeCount += Time.deltaTime;
+                if (_runTimeCount >= _attackTime)
+                {
+                    AtackStart();
+                }
                 break;
 
             case State.Attack:
                 break;
+
+            case State.Search:
+                if (!_foundPlayer)
+                {
+                    _searchTimeCount += Time.deltaTime;
+                    if (_searchTimeCount >= _serachPlayerTime)
+                    {
+                        _animator.SetBool(_isSearch, false);
+                        _searchTimeCount = 0;
+                        if (!CheckPlayerDistance())
+                        {
+                            ChengeState(State.Idle);
+                            _animator.SetBool(_isIdle, true);
+                        }
+                        else
+                        {
+                            FoundPlayer();
+                        }
+                    }
+                }
+                break;
         }
     }
 
-    private void CheckPlayerDistance()
+    private bool CheckPlayerDistance()
     {
-        var distance = Vector3.Distance(_player.transform.position, this.transform.position);
-        if (distance <= _attackDistance)
+        if (!_foundPlayer)
         {
-            ChengeState(State.Move);
+            var playerPos = _player.transform.position;
+            var myPos = this.transform.position;
+            var distance = Vector3.Distance(myPos, playerPos);
+
+            if (distance <= _attackDistance)
+            {
+                FoundPlayer();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+
+        return false;
     }
 
     private void ChengeState(State state)
     {
         _state = state;
+    }
+
+    public void FoundPlayer()
+    {
+        _foundPlayer = true;
+        _animator.SetBool(_isDiscovery, true);
+    }
+
+    public void RunStart()
+    {
+        ChengeState(State.Run);
+        _animator.SetBool(_isDiscovery, false);
+        _animator.SetBool(_isRun, true);
+    }
+
+    public void AtackStart()
+    {
+        ChengeState(State.Attack);
+        _animator.SetBool(_isRun, false);
+        _animator.SetBool(_isAttack, true);
+    }
+
+    public void AttackEnd()
+    {
+        ChengeState(State.Search);
+        _animator.SetBool(_isAttack, false);
+        _animator.SetBool(_isSearch, true);
+        _foundPlayer = false;
+        _runTimeCount = 0f;
     }
 }
