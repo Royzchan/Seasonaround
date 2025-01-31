a518e67aae7f7b5284110c9260908ddf12f35843
林　利樹 <117792737+Royzchan@users.noreply.github.com>
2025-01-31 12:19:18 +0900

    ジャンプの敵を修正
    アニメーションを修正
    死亡処理追加
    ジャンプの挙動を修正

--- Assets/Scripts/GameScene/Enemy/JumpingEnemy.cs
+++ Assets/Scripts/GameScene/Enemy/JumpingEnemy.cs
@@ -1,90 +1,128 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 
 public class JumpingEnemy : MonoBehaviour
 {
     private Rigidbody _rb;
-
-    GameObject player;
-
-    //アニメーター
-    [SerializeField]
+    GameObject _player;
+    PlayerController_2D _playerController;
     private Animator _animator;
-
-    //攻撃のアニメーションの判定
-    private string _isJump = "JumpNow";
 
     [SerializeField, Header("プレイヤーとの距離")]
     private float _distance = 5f;
-
     [SerializeField, Header("ジャンプ力")]
     private float _jumpPower = 5.0f;
-
-    //ジャンプの回数
-    private int _jumpNum = 0;
-
     [SerializeField, Header("ジャンプできる回数")]
     private int _canJumpNum = 1;
+    [SerializeField, Header("ジャンプのクールタイム")]
+    private float _jumpCoolTime = 1.0f;
+    private int _jumpNum = 0; // ジャンプした回数
+    private float _jumpCoolTimeCounter = 0f; // ジャンプのクールタイムのカウンター
+    private bool _canJump = true; // ジャンプ可能フラグ
+    private bool _onGround = true;
 
-    // Start is called before the first frame update
+    //アニメーション
+    private string _isJump = "Jump"; // ジャンプのアニメーションの呼び出しキー
+    private string _isDie = "Die";   // 死亡のアニメーションの呼び出しキー
+
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        _player = GameObject.FindWithTag("Player");
+        _playerController = FindAnyObjectByType<PlayerController_2D>();
         _rb = GetComponent<Rigidbody>();
-        _animator.SetBool(_isJump, false);
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player != null)
+        if (_player != null)
         {
-            float _dis = Vector3.Distance(this.transform.position, player.transform.position);
-            Jump(_dis);
-        }
+            float dis = Vector3.Distance(this.transform.position, _player.transform.position);
+            if (dis <= _distance)
+            {
+                if (_canJump) Jump();
+            }
 
-        if (transform.position.x > player.transform.position.x)
-        {
-            // X軸がプレイヤーより小さい場合、スケールを反転
-            transform.localScale = new Vector3(50f, 50f, 50f);
-        }
-        else
-        {
-            // 元のスケールに戻す
-            transform.localScale = new Vector3(50f, 50f, -50f);
+            if (transform.position.x > _player.transform.position.x)
+            {
+                // X軸がプレイヤーより小さい場合、スケールを反転
+                transform.localScale = new Vector3(50f, 50f, 50f);
+            }
+            else
+            {
+                // 元のスケールに戻す
+                transform.localScale = new Vector3(50f, 50f, -50f);
+            }
         }
     }
 
-    void Jump(float dis)
+    /// <summary>
+    /// ジャンプ処理
+    /// </summary>
+    void Jump()
     {
-        if (dis <= _distance)
+        if (_jumpNum < _canJumpNum && _onGround)
         {
-            //ジャンプする
-            if (_jumpNum < _canJumpNum)
-            {
-                _rb.AddForce(0f, _jumpPower, 0f, ForceMode.Impulse);
-                _jumpNum++;
-                //ジャンプのアニメーション
-                Invoke("JumpAnimation", 0.5f);
-            }
+            _canJump = false;
+            StartCoroutine(StartJumpCoolTime());
+            _animator.SetTrigger(_isJump);
         }
-        else // プレイヤーとの距離が設定した範囲を超えた場合
-        {
-            _animator.SetBool(_isJump, false);
-        }
+    }
+
+    /// <summary>
+    /// ジャンプの力を加える処理
+    /// </summary>
+    public void AddJumpForce()
+    {
+        _rb.AddForce(0f, _jumpPower, 0f, ForceMode.Impulse);
+    }
+
+    private IEnumerator StartJumpCoolTime()
+    {
+        yield return new WaitForSeconds(_jumpCoolTime);
+        _canJump = true;
+    }
+
+    private void Die()
+    {
+        _animator.SetTrigger(_isDie);
+    }
+
+    public void DeleteObj()
+    {
+        Destroy(this.gameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            _jumpNum = 0;
+            //接地したタイミングで地面にいる判定をtrueに
+            _onGround = true;
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (_player.transform.position.y >= this.transform.position.y)
+            {
+                Die();
+            }
+            else
+            {
+                if (_playerController != null) _playerController.Damage(1);
+            }
         }
     }
 
-    void JumpAnimation()
+    private void OnCollisionExit(Collision collision)
     {
-        _animator.SetBool(_isJump, true);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            //地面から離れたら地面にいる判定をfalseに
+            _onGround = false;
+        }
     }
 }
