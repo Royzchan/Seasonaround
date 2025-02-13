238a6da288af448d3f9915a8060f0c362c8d6bbf
Tsuge2022 <hyougotuge2022@gmail.com>
2025-02-13 14:05:48 +0900

    golliraPush修正
    挙動を直しました

--- Assets/Scripts/StageGimmick/GorillaPushObject.cs
+++ Assets/Scripts/StageGimmick/GorillaPushObject.cs
@@ -6,23 +6,30 @@ public class GorillaPushObject : MonoBehaviour
 {
     Rigidbody _rb;
     PlayerController_2D _playerController;
+    Vector3 _prePos;
     // Start is called before the first frame update
     void Start()
     {
+        _prePos = transform.position;
         _playerController = FindAnyObjectByType<PlayerController_2D>();
         _rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        _prePos = transform.position;
+    }
     private void OnCollisionStay(Collision collision)
     {
      
-       if (collision.gameObject.CompareTag("Player"))
+       if(collision.gameObject.CompareTag("Player"))
        {
-           if(_playerController.NowAnimal != Animal.Colobus)
-           {
+            if (_playerController.NowAnimal != Animal.Colobus)
+            {
                 //‰¡•ûŒü‚ÌˆÚ“®‚ðŽ~‚ß‚é
-                _rb.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0); 
-           }
+                _rb.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                transform.position = _prePos;
+            }
        }
     }
 
